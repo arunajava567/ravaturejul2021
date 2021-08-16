6cafabb7907d6daa23dea6f1a04298b1d7211a64
arunajava567 <30722352+arunajava567@users.noreply.github.com>
2021-08-16 13:12:41 +0530

    Add files via upload

---
+++ Demo_AnnoBaseConfig_2/src/main/java/com/cg/anno/CurrencyConverterImpl.java
@@ -0,0 +1,60 @@
+package com.cg.anno;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+@Component("c")                          //<bean id="n" class=""/>
+public class CurrencyConverterImpl //implements CurrencyConverter 
+{
+	
+	@Autowired  // ByName
+	private ExchangeServiceImpl exchangeService;// new    //composition , has a relation
+	
+	
+	//ExchangeServiceImpl eservice=new ExchangeServiceImpl();
+	
+	
+	// currencyConverterImpl 
+	
+	@PostConstruct   //init 
+	void init(){
+		System.out.println("in init() - called thru @PostConstruct");
+	}
+	
+	@PreDestroy  //destroy
+	void destroy(){
+		System.out.println("in destroy() - called thru @PreDestroy");
+	}
+	
+	/*public CurrencyConverterImpl() {
+		System.out.println("CurrencyConverterImpl()");
+	}*/
+	/*@Autowired
+	public CurrencyConverterImpl(ExchangeServiceImpl exchangeService) {
+		System.out.println("CurrencyConverterImpl()");
+		this.exchangeService = exchangeService;
+	}
+	*/
+	
+
+	/*public ExchangeServiceImpl getExchangeService() {
+		System.out.println("getExchangeService()");		
+		return exchangeService;
+	}
+
+	@Autowired
+	public void setExchangeService(ExchangeServiceImpl exchangeService) {
+		System.out.println("setExchangeService()");		
+		this.exchangeService = exchangeService;
+	}*/
+
+	public double dollarsToRupees(double dollars) {
+	//	System.out.println("dollarsToRupees()");	
+	
+		return dollars * exchangeService.getExchangeRate();
+	}
+};
