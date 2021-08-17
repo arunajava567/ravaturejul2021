5035b7c8cce13558a4a7926dbbf79299a88b7051
arunajava567 <30722352+arunajava567@users.noreply.github.com>
2021-08-17 12:53:53 +0530

    Add files via upload

---
+++ aop/AppAOP.java
@@ -0,0 +1,23 @@
+package com.revature.aop;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+
+public class AppAOP {
+	public static void main(String[] args) throws Exception {
+
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("Spring-Customer-AOP.xml");
+
+		CustomerBo customer = (CustomerBo) appContext.getBean("customerBo");
+		//customer.addCustomer();
+		
+		//customer.addCustomerReturnValue();
+		
+		//customer.addCustomerThrowException();
+		
+		customer.addCustomerAround("RevatureUser1");
+
+	}
+}
